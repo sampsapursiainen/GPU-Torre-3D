e7efc6eb2c592be00bc03757a395e94b392ba4f2
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2021-02-17 14:31:35 +0200

    Add files via upload

---
+++ gputorre3d_init.m
@@ -0,0 +1,7 @@
+%Copyright © 2021- Sampsa Pursiainen & GPU-ToRRe-3D Development Team
+%See: https://github.com/sampsapursiainen/GPU-Torre-3D
+
+
+addpath(genpath(pwd));
+[torre_dir] = fileparts(mfilename('fullpath'));
+torre_dir = [torre_dir '/'];
