7695378611fd9f2ed559c396077bffe3f5a16e80
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2021-02-17 14:56:53 +0200

    Add files via upload

--- model_data/Itokawa/itokawa_model.m
+++ model_data/Itokawa/itokawa_model.m
@@ -1,1 +1,4 @@
+[surface_model_directory] = fileparts(mfilename('fullpath'));
+surface_model_stl_file = {'interior_1_dense.stl','itokawa_layer_intermediate.stl','itokawa_v5763_scaled05_neworigin001.stl'};
+surface_model_relative_permittivity = complex([1 4 3],[0 0.02 0.01]);
 
