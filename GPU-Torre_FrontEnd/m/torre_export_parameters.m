daa06039d3a5d6009f740abcd35d816b129131e6
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2021-07-16 11:02:11 +0300

    Add files via upload

--- GPU-Torre_FrontEnd/m/torre_export_parameters.m
+++ GPU-Torre_FrontEnd/m/torre_export_parameters.m
@@ -1,5 +1,5 @@
 if questdlg('Overwrite the current default parameters?'); 
 torre_data = torre.parameters_data;    
-writecell(torre_data,[torre_dir 'parameters_data.dat']);
+writecell(torre_data,[torre_dir 'system_data/parameters_data.dat']);
 clear torre_data;
 end
