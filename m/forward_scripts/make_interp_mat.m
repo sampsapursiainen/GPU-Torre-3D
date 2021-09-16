bc8aa20fb2c01b72b1219d7482f71977d36ec0c6
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2021-09-16 14:25:43 +0300

    Add files via upload

--- m/forward_scripts/make_interp_mat.m
+++ m/forward_scripts/make_interp_mat.m
@@ -9,7 +9,9 @@ lattice_resolution = round(4*lattice_oversampling_rate*(signal_highest_frequency
 I_aux = find(abs(nodes(:,1))<interpolation_radius & abs(nodes(:,2))<interpolation_radius & abs(nodes(:,3))<interpolation_radius);
 interp_ind = zeros(length(I_aux),3);
 [x_lattice_interpolation,y_lattice_interpolation,z_lattice_interpolation] = meshgrid([-interpolation_radius + interpolation_radius/(lattice_resolution): 2*interpolation_radius/(lattice_resolution):interpolation_radius-interpolation_radius/(lattice_resolution)]);
-interp_ind_aux = griddata(nodes(I_aux,1),nodes(I_aux,2),nodes(I_aux,3),[1:length(I_aux)]',x_lattice_interpolation,y_lattice_interpolation,z_lattice_interpolation,'nearest');
-Interp_mat = sparse([1:lattice_resolution^3]',interp_ind_aux(:),ones(lattice_resolution^3,1),lattice_resolution^3,size(nodes,1));
+center_points = [x_lattice_interpolation(:) y_lattice_interpolation(:) z_lattice_interpolation(:)];
+MdlKDT = KDTreeSearcher(nodes(I_aux,:));
+aux_ind_vec = knnsearch(MdlKDT,center_points);
+Interp_mat = sparse([1:length(x_lattice_interpolation(:))]', I_aux(aux_ind_vec),ones(length(x_lattice_interpolation(:)),1),length(x_lattice_interpolation(:)),size(nodes,1));
 
 save([torre_dir '/system_data/interp_mat_' int2str(system_setting_index) '.mat'], 'Interp_mat', 'x_lattice_interpolation', 'y_lattice_interpolation', 'z_lattice_interpolation', '-v7.3');
