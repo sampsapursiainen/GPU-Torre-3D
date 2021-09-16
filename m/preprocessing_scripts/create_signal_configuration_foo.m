841159c8f184cc6abd2388fc325ace35f505f18b
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2021-09-16 10:36:45 +0300

    Add files via upload

--- m/preprocessing_scripts/create_signal_configuration_foo.m
+++ m/preprocessing_scripts/create_signal_configuration_foo.m
@@ -1,6 +1,8 @@
+parameters;
+
 path_data = [1 1];
 source_index = [1];
-source_orientations = [1 1 1]/sqrt(3);
-source_points = 2.5353*[1 1 1]/sqrt(3);
+source_orientations = [1 0 -1]/sqrt(3);
+source_points = or_radius*[1 1 1]/sqrt(3);
 source_sub_index = [0 1];
 save system_data/signal_configuration.mat path_data source_index source_orientations source_points source_sub_index;
