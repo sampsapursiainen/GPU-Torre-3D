2bb80081b85fc17799314f6f43dd0cf52717fe47
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2021-09-16 10:39:17 +0300

    Add files via upload

--- m/forward_scripts/compute_data.m
+++ m/forward_scripts/compute_data.m
@@ -1,10 +1,15 @@
 %Copyright © 2021- Sampsa Pursiainen & GPU-ToRRe-3D Development Team
 %See: https://github.com/sampsapursiainen/GPU-Torre-3D
 
+parameters;
+
+if not(exist('gpu_id'))
+gpu_id = default_gpu_id;
+end
 
 max_plot_val = 0;
 gpuDevice(gpu_id);
-parameters;
+
 
 if compute_in_pieces == 1
 time_series_id_vec = [time_series_id];
