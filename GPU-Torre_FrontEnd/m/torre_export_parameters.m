88d1bd24f2ef54b7dab9be44125b7e3fa15f02db
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2021-09-21 17:40:55 +0300

    Update torre_export_parameters.m

--- GPU-Torre_FrontEnd/m/torre_export_parameters.m
+++ GPU-Torre_FrontEnd/m/torre_export_parameters.m
@@ -1,5 +1,5 @@
 if questdlg('Overwrite the current default parameters?'); 
 torre_data = torre.parameters_data;    
-save([torre_dir torre.system_data_folder '/parameters_data.mat'],torre_data);
+save([torre_dir torre.system_data_folder '/parameters_data.mat'],'torre_data');
 clear torre_data;
 end
